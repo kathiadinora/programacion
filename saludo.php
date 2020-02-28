b8ea1d9e7d9d21318f4db1c39676f42d355cf3b1
kathiadinora <kathiadinora21@gmail.com>
2020-02-27 20:10:41 -0600

    Saludo con ajax

--- saludo.php
+++ saludo.php
@@ -2,8 +2,10 @@
 date_default_timezone_set('America/El_Salvador');
 
 if( $_GET && isset($_GET['name']) ){
-$nombre = $_GET['name']; 
-echo 'Hola' .$nombre. ' desde el servidor, fecha: '.date('d-m-Y H:i:s);
+    $nombre = $_GET['name'];
+    echo 'Hola '.$nombre. 
+        ' desde el servidor, fecha: '.
+        date('d-m-Y H:i:s');
 } else{
     header('location:index.html');
 }
